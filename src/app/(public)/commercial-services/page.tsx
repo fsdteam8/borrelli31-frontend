efccb839ac7f0c0d32075d76c6e78de86cd05328
undefinedSahed <sahedrahmanshakil@gmail.com>
2025-08-26 06:53:45 +0600

    added the commercial page banner

---
+++ src/app/(public)/commercial-services/page.tsx
@@ -0,0 +1,10 @@
+import CommercialBanner from "@/components/features/public/commercial/cs-banner";
+import React from "react";
+
+export default function page() {
+  return (
+    <main>
+      <CommercialBanner />
+    </main>
+  );
+}
